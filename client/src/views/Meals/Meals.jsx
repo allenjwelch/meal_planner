b45ebefd0be0dee17073e680a7a3204596659efd
Allen Welch <allenjwelch@gmail.com>
2019-02-24 17:12:30 -0500

    added styling and edit btn

--- client/src/views/Meals/Meals.jsx
+++ client/src/views/Meals/Meals.jsx
@@ -36,8 +36,30 @@ class Meals extends Component {
     } 
 
     getMealPlan() {
+        //! TODO: if local storage has meal plan && if new week
         let meals = this.shuffleMeals(this.state.allMeals).slice(0, 7); 
         this.setState({mealPlan: meals}, () => {console.log(this.state.mealPlan, "state.mealPlan")})
+    }
+
+    reorderMealPlan(pos1, pos2) {
+        let newMealPlan = this.state.mealPlan; 
+        let temp = newMealPlan[pos2]; 
+        newMealPlan[pos2] = newMealPlan[pos1]; 
+        newMealPlan[pos1] = temp; 
+        console.log(newMealPlan);
+    }
+
+    enableEdits() {
+        console.log('editing...');
+        let meals = document.querySelectorAll('.display li'); 
+        meals.forEach(node => {
+            node.classList.add('editing'); 
+            node.addEventListener('click', (e) => {
+                console.log(e.target); 
+            })
+            console.log(node); 
+        })
+        console.log(meals); 
     }
 
 
@@ -46,10 +68,29 @@ class Meals extends Component {
             <section className="main">
                 {
                     this.state.allMeals.length ?
+                    
+                        <div className="meal-plan">
+                            <img src={logo} alt="logo" />
+                            <h1>Welcome User!</h1>
+                            {/* <ul className='days'>
+                                <li>Sun</li>
+                                <li>sat</li>
+                            
+                            </ul> */}
+                            <ul className="display">
+                                {
+                                    this.state.mealPlan.map( (meal, index) => {
+                                        return <li key={meal.id} data-day={index}>{meal.name}</li>
+                                    })
+                                }
+                            </ul>
 
-                        this.shuffleMeals(this.state.allMeals).map(meal => {
-                            return <li key={meal.id}>{meal.name}</li>
-                        })
+
+                            <div className="actions">
+                                <button className="edit" onClick={() => this.enableEdits()}>Edit</button>
+                            </div>
+                        </div>
+
 
                     : <img src={logo} className="logo blank-page" alt="logo" />
                 }
--- routes/api/meals.js
+++ routes/api/meals.js
@@ -16,6 +16,38 @@ router.get("/", function(req, res) {
             id: 2,
             name: "meal 2", 
         }, 
+        {
+            id: 3,
+            name: "meal 3", 
+        },
+        {
+            id: 4,
+            name: "meal 4", 
+        },
+        {
+            id: 5,
+            name: "meal 5", 
+        },
+        {
+            id: 6,
+            name: "meal 6", 
+        },
+        {
+            id: 7,
+            name: "meal 7", 
+        },
+        {
+            id: 8,
+            name: "meal 8", 
+        },
+        {
+            id: 9,
+            name: "meal 9", 
+        },
+        {
+            id: 10,
+            name: "meal 10", 
+        }
     ]);
 });
 
