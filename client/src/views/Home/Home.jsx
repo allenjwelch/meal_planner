3ad46865851a59b864e240a8bc023dc298e79c7b
Allen Welch <allenjwelch@gmail.com>
2019-02-27 14:22:33 -0500

    began working on setting local storage and sign in feature

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -9,19 +9,48 @@ class Home extends Component {
 
     state = {
         user: '', 
+        newUser: false,
     }
 
     componentDidMount() {
         this.getUser()
+        // this.checkStorage()
     }
 
     getUser() {
-        API.getUser()
-            .then(res => 
-                this.setState({user: res.data, }, () => {
-                    console.log(this.state.user, "state.user"); 
-                }))
-            .catch(err => console.log(err))
+        if (this.state.newUser) {
+            // API.postUser()
+        } else {
+            API.getUser()
+                .then(res => 
+                    this.setState({user: res.data, }, () => {
+                        console.log(this.state.user, "state.user");
+                        localStorage.setItem('user', this.state.user[0].id)
+                    }))
+                // .then( localStorage.setItem('user', this.state.user[0]))
+                .catch(err => console.log(err))
+        }
+    }
+
+    checkStorage() {
+        if(localStorage.getItem('user')) {
+            let user = localStorage.getItem('user'); 
+            console.log(user); 
+        } else {
+            console.log('no user signed in')
+        }
+    }
+
+    validateUser() {
+        if (true) {
+
+        }
+
+
+    }
+
+    swapSignIn() {
+        this.state.newUser ? this.setState({newUser: false }) : this.setState({newUser: true});
     }
 
     render() {
@@ -39,12 +68,28 @@ class Home extends Component {
                     : <div className="no-access">
                         <section className="sign-in">
                             <img src={logo} className="logo" alt="logo" />
-                            <p>SignIn</p>
-                            <form action="" method="POST">
-                                <input type="text" placeholder="User"/>
-                                <input type="password" placeholder="Password"/>
-                                <input type="submit" value="Submit"/>
-                            </form>
+
+                            {
+                                !this.state.newUser ?
+                                    <div className="returning-user">
+                                        <p>SignIn</p>
+                                        <form action="">
+                                            <input id="user-name" type="text" placeholder="User"/>
+                                            <input id="user-password" type="password" placeholder="Password"/>
+                                            <button onClick={() => this.validateUser()}>Submit</button>
+                                        </form>
+                                        <p className="swap-sign-in" onClick={() => this.swapSignIn()}>New User?</p>
+                                    </div>
+                                : <div className="new-user">
+                                        <p>Register</p>
+                                        <form action="">
+                                            <input id="user-name" type="text" placeholder="User"/>
+                                            <input id="user-password" type="password" placeholder="Password"/>
+                                            <button onClick={() => this.validateUser()}>Submit</button>
+                                        </form>
+                                        <p className="swap-sign-in" onClick={() => this.swapSignIn()}>Returning User?</p>
+                                </div>
+                            }
                         </section>
                         <div className="overlay"></div>
                     </div>
