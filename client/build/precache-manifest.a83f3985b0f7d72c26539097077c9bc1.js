90637db41c3ff8dcbc063f9c1657758a83f5bd5c
Allen Welch <allenjwelch@gmail.com>
2019-03-08 13:33:53 -0500

    removed /build from gitignore

---
+++ client/build/precache-manifest.a83f3985b0f7d72c26539097077c9bc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9cadaf438810835948259793ecd8dcbc",
+    "url": "/static/media/logo.9cadaf43.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "505c904c6280b82b7789",
+    "url": "/static/js/main.505c904c.chunk.js"
+  },
+  {
+    "revision": "23bef6ae2d641e4746e5",
+    "url": "/static/js/2.23bef6ae.chunk.js"
+  },
+  {
+    "revision": "505c904c6280b82b7789",
+    "url": "/static/css/main.5cb11f97.chunk.css"
+  },
+  {
+    "revision": "58c5a255dad16e03223b864e53202c58",
+    "url": "/index.html"
+  }
+];
